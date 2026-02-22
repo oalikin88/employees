52d6f05847c8b2979a0da9a1f3531081fa41be0d
Oleg Alikin <user@WIN-T4LD0UJ1RIJ>
2026-02-22 12:02:05 +0300

    Создание rest контроллера employee

--- src/main/java/ru/aos/employees/interfaces/EmployeeMapper.java
+++ src/main/java/ru/aos/employees/interfaces/EmployeeMapper.java
@@ -2,6 +2,7 @@ package ru.aos.employees.interfaces;
 
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import ru.aos.employees.entity.Employee;
@@ -15,7 +16,9 @@ import ru.aos.employees.models.EmployeeRequest;
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 
+    @Mapping(source = "department.id", target = "departmentId")
     EmployeeDto toDto(Employee employee);
+    @Mapping(source = "departmentId", target = "department.id")
     Employee toEntity(EmployeeRequest request);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromRequest(EmployeeRequest request, @MappingTarget Employee employee);
