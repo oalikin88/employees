672ac2c3b29c3f8d7a064bf2ae9ffa90dac92f14
Oleg Alikin <user@WIN-T4LD0UJ1RIJ>
2026-02-23 09:52:20 +0300

    понижение версий springboot и lombok. Создание класса EmployeeClient

--- src/main/java/ru/aos/employees/EmployeesApplication.java
+++ src/main/java/ru/aos/employees/EmployeesApplication.java
@@ -2,8 +2,10 @@ package ru.aos.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EmployeesApplication {
 
 	public static void main(String[] args) {
